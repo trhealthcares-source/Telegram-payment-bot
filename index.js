66d8a865c4791f553637eb2080dbe5cfe51c60f1
trhealthcares-source <trhealthcares@gmail.com>
2025-12-25 21:17:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 require("./telegram");
+require("./reminder");
 const webhook = require("./webhook");
 
 const app = express();
 app.use(bodyParser.json());
 app.use("/", webhook);
 
-app.get("/", (req, res) => res.send("Bot running"));
+app.get("/", (_, res) => res.send("Bot running"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server started"));
+app.listen(PORT);
