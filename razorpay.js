c2a5b2051558df55b9a30d4d0c5aec199ffa75c1
trhealthcares-source <trhealthcares@gmail.com>
2025-12-25 22:04:56 +0530

    Update razorpay.js

--- razorpay.js
+++ razorpay.js
@@ -1,6 +1,8 @@
 const Razorpay = require("razorpay");
 
-module.exports = new Razorpay({
+const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
+
+module.exports = razorpay;
